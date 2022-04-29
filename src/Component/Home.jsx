55458209fce3cdaf8bb2070134c9bbce53ea45c4
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-04-29 17:57:04 +0530

    Font AweSome icons added and remove update link from nav

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,31 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faList12,
+  faSearch,
+  faHomeUser,
+  faHeartBroken,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <>
-    
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" >
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/List">RestroList</Link>
-          </li>
-          
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">Search</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/update/:id">Update</Link>
-          </li>
-          <li className='nav-item'><Link className="nav-link" to='/create'>Create</Link></li>
-        </ul>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FontAwesomeIcon icon={faHomeUser} color="blue" />
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">
+                <FontAwesomeIcon icon={faHeartBroken} color="blue" />
+                Create
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/List">
+                <FontAwesomeIcon icon={faList12} color="blue" />
+                RestroList
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/search">
+                <FontAwesomeIcon icon={faSearch} color="blue" />
+                Search
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/update/:id"></Link>
+            </li>
+          </ul>
         </div>
       </nav>
-     
     </>
   );
 };
--- src/Component/RestoList.jsx
+++ src/Component/RestoList.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEdit,faTrash} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const RestoList = () => {
   const [list, setList] = useState([]);
@@ -25,12 +23,12 @@ const RestoList = () => {
   return (
     <>
       <div className="App">
-        <h1>Restaurant List</h1>
+        <h1 className="text-white  bg-info">Restaurant List</h1>
       </div>
       <div>
-        <table className="table table-hover table-dark table-sm">
+        <table className="table table-hover table-dark">
           <thead className="bg-success">
-            <tr >
+            <tr>
               <th className="bg-danger">Id</th>
               <th className="bg-info">name</th>
               <th>Address</th>
@@ -39,30 +37,30 @@ const RestoList = () => {
               <th className="bg-warning">Opration</th>
             </tr>
           </thead>
-      <tbody>
-
-     
-        {
-          list.map((ele)=>{
-            return(
-              <tr key={ele.id}>
-                <td>{ele.id}</td>
-                <td>{ele.name}</td>
-                <td>{ele.address}</td>
-                <td>{ele.rating}</td>
-                <td>{ele.email}</td>
-                <td> <Link  to={"/update/"+ele.id}><FontAwesomeIcon icon={faEdit }color='yellow'/></Link>
-                <Link  to={"/update/"+ele.id}><FontAwesomeIcon icon={faTrash }color='red'/></Link></td>
-              </tr>
-             
-            )
-
-          })
-        }
-         </tbody>
-          </table>
+          <tbody>
+            {list.map((ele) => {
+              return (
+                <tr key={ele.id}>
+                  <td>{ele.id}</td>
+                  <td>{ele.name}</td>
+                  <td>{ele.address}</td>
+                  <td>{ele.rating}</td>
+                  <td>{ele.email}</td>
+                  <td>
+                   
+                    <Link to={"/update/" + ele.id}>
+                      <FontAwesomeIcon icon={faEdit} color="yellow" />
+                    </Link>
+                    <Link to={"/update/" + ele.id}>
+                      <FontAwesomeIcon icon={faTrash} color="red"  />
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-    
     </>
   );
 };
