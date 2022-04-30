825432c18355878fd73a714d804ff4dd5e0f3682
SukhdebPatra <sukhdebpatra1@gmail.com>
2022-04-30 19:51:50 +0530

    list , style added in search compo

--- src/Component/RestoSearch.jsx
+++ src/Component/RestoSearch.jsx
@@ -1,9 +1,11 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 
 const RestoSearch = () => {
-  
-const [searchData, setSearchData] = useState('')
-
+  const [searchData, setSearchData] = useState("");
 
   // const search=(key)=>{
   //   console.log(key);
@@ -16,48 +18,76 @@ const [searchData, setSearchData] = useState('')
 
   // }
 
-  const serachHandler=async(e)=>{
+  const serachHandler = async (e) => {
     console.log(e.target.value);
-    let key =e.target.value;
-    let result=await fetch(`http://localhost:3000/restaurant?q=${key}`)
-    result= await result.json();
-    if(result){
-      setSearchData(result)
+    let key = e.target.value;
+    let result = await fetch(`http://localhost:3000/restaurant?q=${key}`);
+    result = await result.json();
+    if (result) {
+      setSearchData(result);
     }
-  }
+  };
   return (
     <div>
-    
       <h1> RestoSearch</h1>
       {/* <input type="text" onChange={(e)=>search(e.target.value)} /> */}
       <div>
-      <input type="text" onChange={serachHandler} />
+        <input
+          className="form-control"
+          aria-label="Small"
+          placeholder="Search Restro"
+          type="text"
+          onChange={serachHandler}
+        />
       </div>
       <div>
-        {
-          searchData ?
-          <div>
-            {
-           searchData.map((item)=>{
-             return (
-              <ul key={item.id}>
-                <li>{item.name}</li>
-              </ul>
-
-              
-
-             )
-               
-              })
-            }
-            </div>
-            : null
-        }
+       
+            {searchData ? (
+              <div>
+                 <table className="table table-hover table-dark">
+          <thead className="bg-success">
+            <tr>
+              <th className="bg-danger">Id</th>
+              <th className="bg-info">name</th>
+              <th>Address</th>
+              <th className="bg-warning">Rating</th>
+              <th className="bg-primary">Email</th>
+              <th className="bg-warning">Opration</th>
+            </tr>
+          </thead>
+          <tbody>
+                {searchData.map((ele) => {
+                  return (
+                    <tr key={ele.id}>
+                    <td>{ele.id}</td>
+                  <td>{ele.name}</td>
+                  <td>{ele.address}</td>
+                  <td>{ele.rating}</td>
+                  <td>{ele.email}</td>
+                  <td>
+                   
+                   <Link cursor='pointer'  className="px-5" to={"/update/" + ele.id}>
+                     <FontAwesomeIcon icon={faEdit} color="yellow" />
+                   </Link>
+                   <span  className="px-5" onClick={()=>deleteUser(ele.id)}>
+                     <FontAwesomeIcon icon={faTrash} color="red"  />
+                   </span>
+                 </td>
+                 
+                     
+                    </tr>
+                   
+                  );
+                 
+                })}
+                 </tbody>
+                  </table>
+              </div>
+            ) : null}
+         
       </div>
-      
     </div>
   );
 };
-
 
 export default RestoSearch;
